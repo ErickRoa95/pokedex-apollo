2ecc1352ce25167bcc93d417e4f98a5de1bda754
ErickRodriguezDev <erick.rodriguez@wizeline.com>
2025-10-14 15:40:03 -0700

    ADD: Implementation Apollo Server with middleware.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,30 @@
 import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import typeDefs from './data/graphql/typeDefs';
+import resolvers from './data/graphql/resolvers';
 
-let name : string = "Erick";
 
-const app = express();
-const PORT= "4000";
 
-app.get("/health", (_, res) => res.send("Server OK"));
+async function startApolloServer(){
+  const app = express();
+  const PORT= "4000";
 
-app.listen(PORT, ()=>{
-  console.log("Server ready  at http://localhost:4000 !!");
-});
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  // start apollo server
+  await server.start();
+  // apply middle ware of apollo server (in this case Express endpoints.)
+  server.applyMiddleware({app});  
+
+  app.get("/health", (_, res) => res.send("Server OK"));
+
+  // Express server call. 
+  app.listen(PORT, ()=>{
+    console.log(`Server ready at http://localhost:${PORT}/${server.graphqlPath}`);
+  });
+}
+
+startApolloServer();
