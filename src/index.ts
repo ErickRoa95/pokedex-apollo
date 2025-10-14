f8791160df2366f6e22a2508e3ccd713b89b06f8
ErickRodriguezDev <erick.rodriguez@wizeline.com>
2025-10-14 13:14:23 -0700

    ADD: Express app initialization.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-let name : string = "Erick Ivan";
-console.log(`Hello, ${name}`);
+import express from 'express';
+
+let name : string = "Erick";
+
+const app = express();
+const PORT= "4000";
+
+app.get("/health", (_, res) => res.send("Server OK"));
+
+app.listen(PORT, ()=>{
+  console.log("Server ready  at http://localhost:4000 !!");
+});
